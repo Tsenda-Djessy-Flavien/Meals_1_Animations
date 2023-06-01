e8551553b96060252d283ed7bdefd11eed177b9f
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-01 14:02:40 +0100

    Réglage fin des animations explicites

--- lib/presentation/pages/categories_screen.dart
+++ lib/presentation/pages/categories_screen.dart
@@ -89,9 +89,22 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             ),
         ],
       ),
-      builder: (context, child) => Padding(
-        // start value is 0 => 0*100 = 0
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      builder: (context, child) => SlideTransition(
+        // position: _animationController.drive(
+        //   Tween(
+        //     begin: const Offset(0, 0.3),
+        //     end: const Offset(0, 0),
+        //   ),
+        // ),
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
         child: child,
       ),
     );
